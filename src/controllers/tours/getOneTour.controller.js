0d098a2c6f64173d54551f9486c202b9e8b166cf
fritz <jhonafritz@gmail.com>
2023-04-28 23:49:14 -0500

    agragdo la ruta para obtener un solo registro de tour

---
+++ src/controllers/tours/getOneTour.controller.js
@@ -0,0 +1,39 @@
+import { pool } from "../../dbconection/db.js";
+
+export const getOneTour = async (req, res) => {
+  try {
+    const [rows] = await pool.query(
+      `SELECT tour.*, imagenes.*
+      FROM tour
+      INNER JOIN imagenes
+      ON tour.id_tour = imagenes.tour_id_tour WHERE id_tour = ?`,
+      [req.params.id]
+    );
+    if (!rows.length)
+      return res.status(404).json({ msg: "Tour no encontrado" });
+
+      const tour = {
+        id_tour: rows[0].id_tour,
+        nombre_tour: rows[0].nombre_tour,
+        precio_tour: rows[0].precio_tour,
+        precio_promo_tour: rows[0].precio_promo_tour,
+        duracion_tour: rows[0].duracion_tour,
+        descripcion: rows[0].descripcion,
+        estado_tour: rows[0].estado_tour,
+        imagenes: [],
+      };
+      for (let i = 0; i < rows.length; i++) {
+        const imagen = {
+          id_img: rows[i].id_img,
+          nombre_img: rows[i].nombre_img,
+          url_img: rows[i].url_img,
+          tour_id_tour: rows[i].tour_id_tour,
+        };
+        tour.imagenes.push(imagen);
+      }
+      res.json(tour);
+
+  } catch (error) {
+    return res.status(500).json({ msg: "Error al obtener el tour" });
+  }
+};
--- src/routes/tours.routes.js
+++ src/routes/tours.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 //importar middlewares para subir imagenes con multer
 import { multerUpload } from "../middlewares/multer.js";
-import { addNewTour } from "../controllers/tours/addNewTour.controller.js"; // ES6 import syntax (Node.js 14+) cuando se usa el export default y es un modulo creado por nosotros y no uno de node.js o de una libreria de terceros como express o body-parser debemos usar la extension .js
+import { addNewTour } from "../controllers/tours/addNewTour.controller.js";
+import {getOneTour} from '../controllers/tours/getOneTour.controller.js' // ES6 import syntax (Node.js 14+) cuando se usa el export default y es un modulo creado por nosotros y no uno de node.js o de una libreria de terceros como express o body-parser debemos usar la extension .js
 
 const router = Router();
 // router.get("/tours", getTours);
 
-// router.get("/tours/:id", getTour);
+router.get("/tour/:id", getOneTour);
 
 router.post("/tour", multerUpload.single("image"), addNewTour, (req, res) => { // "image" is the name of the input field in the form
   console.log(req.file);
